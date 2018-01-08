e34cd56e6f6d5687a821f71ae91b62730badbd3f
Junchao Chen <csammy@siu.edu>
2018-01-08 08:43:14 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import './App.css';
 
 class App extends Component {
   render() {
+  	var helloworld = "First React App";
+  	var secondVariable = "the 2nd variable";
+  	let thirdVariable = "The thirrd variable";
+  	const nonmutatedVariable = "Does not change";
+
+
+
+  	thirdVariable = "Just got changed";
+
     return (
     	<div className = "MyApp">
-    		<h2>Version0.0000</h2>
+    		<h2>{helloworld}</h2>
+    		<h1>{secondVariable}</h1>
+    		<h4>{thirdVariable}</h4>
+    		<h1>{nonmutatedVariable}</h1>
+
     	</div>
     );
   }
