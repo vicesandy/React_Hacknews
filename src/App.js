4abc2f9be1a6c910d43ed8494ad9eab44cd10ae1
Junchao Chen <csammy@siu.edu>
2018-01-07 20:23:08 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    	<div className = "MyApp">
+    		<h2>Version0.0000</h2>
+    	</div>
     );
   }
 }
