a0f757d95af9a0cc9701ffd4e34592ab920c03b1
Junchao Chen <csammy@siu.edu>
2018-01-09 09:38:57 -0800

    3commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+  const list = [
+  		{
+  			title: 'myReact',
+  			url: '//facebook.github.io/react/',
+  			author: 'J.Chen',
+  			num_comments: 3,
+  			points: 4,
+  			objectID: 0,
+  		},
+
+  		{
+  			title: 'myRedux',
+  			url: '//github.com/reactjs/redux',
+  			author: 'D Abramov, A Clark',
+  			num_comments: 2,
+  			points: 5,
+  			objectID: 1,
+  		},
+  ];
+
 class App extends Component {
   render() {
-  	var helloworld = "First React App";
-  	var secondVariable = "the 2nd variable";
-  	let thirdVariable = "The thirrd variable";
-  	const nonmutatedVariable = "Does not change";
-
-
-
-  	thirdVariable = "Just got changed";
-
     return (
     	<div className = "MyApp">
-    		<h2>{helloworld}</h2>
-    		<h1>{secondVariable}</h1>
-    		<h4>{thirdVariable}</h4>
-    		<h1>{nonmutatedVariable}</h1>
+    	{list.map(function(item){
+    		return (
+    			<div>
+    			<span>
+    				<a href={item.url}>{item.title}</a>
+    			</span>
+    			<span>{item.points}</span>
+    			<span>{item.num_comments}</span>
+    			<span>{item.author}</span>
+    			</div>
+
+    			);
+    	})}
+
 
     	</div>
     );
