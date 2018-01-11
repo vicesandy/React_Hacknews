320978945c50544c522448fbfb131af09fbd1410
Junchao Chen <csammy@siu.edu>
2018-01-10 22:19:08 -0800

    4

--- src/App.js
+++ src/App.js
@@ -32,11 +32,32 @@ import './App.css';
   		},
   ];
 
+  class Developer {
+  		constructor(firstname, lastname){
+  			this.firstname = firstname;
+  			this.lastname = lastname;
+  		}
+  		getName() {
+  			return this.firstname + '' + this.lastname
+  		}
+  }
+
+  const sam = new Developer('Sam', 'Chen');
+  console.log(sam.getName);
+
 class App extends Component {
+  constructor(props){
+  	super(props);
+
+  	this.state = {
+  		list: list,
+  	};
+  }
+
   render() {
     return (
     	<div className = "MyApp">
-	    	{list.map(item =>
+	    	{this.state.list.map(item =>
 	    			<div key={item.objectID}>
 	    			<span>
 	    				<a href={item.url}>{item.title}</a>
