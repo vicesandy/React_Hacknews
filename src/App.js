59a4e36d62b3a489273a0a6626315e6807ec85ca
Junchao Chen <csammy@siu.edu>
2018-02-02 12:29:59 -0600

    componenet

--- src/App.js
+++ src/App.js
@@ -91,17 +91,18 @@ class Table extends Component {
   render() {
     const {list, pattern, onDismiss} = this.props;
       return (
-        <div>
+        <div className="table">
           {list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID}>
-              <span>
+            <div key={item.objectID} className="table-row">
+              <span style={{ width: '40%' }}>
                 <a href={item.url}>{item.title}</a>
               </span>
-              <span>{item.author}</span>
+              <span style={{ width: '30%' }}>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
               <span>
-                <Button onClick={() => onDismiss(item.objectID)}>
+                <Button onClick={() => onDismiss(item.objectID)}
+                        className="button-inline">
                   DisMiss
                 </Button>
               </span>
